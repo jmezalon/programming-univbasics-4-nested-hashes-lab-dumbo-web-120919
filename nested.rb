2149b2abf9efe49cde77b0778e567d61d874280f
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 15:20:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,7 +133,8 @@ def adding_to_dennis
         }
      }
      
-   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
+  programmer_hash
 
 end
